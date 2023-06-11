65e55c1250a6a7857411a7506c7802e4bba62878
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-11 08:39:34 +0530

    adding middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log("HELLO WORLD");
+import express from "express";
+import { connectDB } from "./db/connect";
+import * as dotenv from "dotenv";
+import { json } from "body-parser";
+import { notFound } from "./middleware/not-found";
+import { errorHandlerMiddleware } from "./middleware/error-handler";
+import "express-async-errors"
+dotenv.config();
+const app = express();
+
+// middleware
+app.use(json());
+app.get("/", (req, res) => {
+  res.send("e-commerce-api");
+});
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3001;
+const mongo_url: string | undefined = process.env.MONGO_URL;
+const start = async () => {
+  try {
+    if (mongo_url) {
+      await connectDB(mongo_url);
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    }
+  } catch (error) {}
+};
+start();
